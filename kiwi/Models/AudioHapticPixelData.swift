3d815de2c6ee1e17e94dfbb2b18c1a5c92b637dc
hugofloresgarcia <hf01049@georgiasouthern.edu>
2022-04-26 19:38:26 -0500

    remove old pixel data code

--- kiwi/Models/AudioHapticPixelData.swift
+++ kiwi/Models/AudioHapticPixelData.swift
@@ -133,60 +133,6 @@ final class PixelCollection: ObservableObject, RandomAccessCollection {
     }
 }
 
-//
-//final class PixelData: ObservableObject {
-//    @ObservedObject var osc: OSC = .shared
-//    @Published var pixels: OrderedDictionary<Int, AudioHapticPixel> = [:]
-//
-//    // assumes that array of pixels has continously increasing ids
-//    // TODO: use an orderedDict instead and just sort after insertion
-//    func addPixels(_ newPixels: inout [AudioHapticPixel]) {
-//        for pixel in newPixels {
-//            self.pixels[pixel.id] = pixel
-//        }
-//
-////        self.pixels.sort() { pix1, pix2 in
-////            return pix1.1.id < pix2.1.id
-////        }
-//    }
-//
-//    func safeIndex(_ at: Int) -> AudioHapticPixel {
-//        let res = self.pixels.elements[safe: at] ?? (at, AudioHapticPixel(id:0, value:0))
-//        return res.1
-//    }
-//
-//    func prepare() {
-//        let stat: Bool = true
-//        osc.send(Bool.convert(values: stat.values), at: "/init") // ??
-//
-//        osc.receive(on: "/pixel", { values in
-//            let pixelsStr: String = .convert(values: values)
-//            let newPixel: AudioHapticPixel = loadFromString(pixelsStr) ?? AudioHapticPixel.init(id: 0, value: 0)
-//            var newPixels = [newPixel]
-//
-//            DispatchQueue.main.async {
-//                self.addPixels(&newPixels)
-//            }
-//
-//        })
-//
-//        osc.receive(on: "/pixels", { values in
-//            let pixelsStr: String = .convert(values: values)
-//            var newPixels: [AudioHapticPixel] = loadFromString(pixelsStr) ?? []
-//
-//            DispatchQueue.main.async {
-//                self.addPixels(&newPixels)
-//            }
-//        })
-//
-//        osc.receive(on: "/pixels/clear", {values in
-//            DispatchQueue.main.async {
-//                self.pixels.removeAll(keepingCapacity: true)
-//            }
-//        })
-//    }
-//}
-
 func loadFromFile<T: Decodable>(_ filename: String) -> T {
     let data: Data
 
